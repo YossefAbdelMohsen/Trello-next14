5367af5f820389c399ae83df4457db7fb4ef1bf8
YossefAbdelMohsen <yossefmohamed112233@gmail.com>
2023-12-29 23:06:46 +0200

    Add authentication and platform layout components - Added .env file to gitignore - Created new authentication components for sign-in and sign-up - Implemented platform layout and protected page components - Added middleware for authentication handling - Updated "@clerk/nextjs" dependency to version 4.29.1 - Introduced "authMiddleware" for protecting routes - Included support for public routes - Included config for route matching - Added new dependent packages in package.json - These changes lay the foundation for improved user authentication and platform structuring. They establish a robust groundwork for future functionality and facilitate a seamless authentication experience for users.

---
+++ app/(platform)/protected/page.tsx
@@ -0,0 +1,11 @@
+import { UserButton, useAuth, useUser } from "@clerk/nextjs";
+
+const ProtectedPage = () => {
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+};
+
+export default ProtectedPage;
